2c735d4b0b29726d8a30f75b1ee64a3f248f14b8
kimchanhoo <cksgn0709@gmail.com>
2021-11-11 22:37:11 +0900

    pin 기능 구현

--- SeSacMemo/MemoApp/ContentViewController.swift
+++ SeSacMemo/MemoApp/ContentViewController.swift
@@ -6,10 +6,15 @@
 //
 
 import UIKit
+import RealmSwift
 
 class ContentViewController: UIViewController {
 
 	@IBOutlet weak var contentTextView: UITextView!
+	
+	let localRealm = try! Realm()
+	
+	var text: String? = nil
 	
 	override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,7 +24,7 @@ class ContentViewController: UIViewController {
 		
 		let shareButton = UIBarButtonItem(title: nil, style: .plain, target: self, action: nil)
 		shareButton.image = UIImage(systemName: "square.and.arrow.up")
-		let finishButton = UIBarButtonItem(title: "완료", style: .plain, target: self, action: nil)
+		let finishButton = UIBarButtonItem(title: "완료", style: .plain, target: self, action: #selector(finishButtonClicked(_:)))
 		
 		navigationItem.setRightBarButtonItems([finishButton,shareButton], animated: true)
 		
@@ -27,7 +32,40 @@ class ContentViewController: UIViewController {
 		
 		 
     }
-
+	
+	func realmSave() {
+		let memoTitle: String
+		let memoContent: String
+		let date = "20211111"
+		let splitText = contentTextView.text.split(separator: "\n", maxSplits: 1)
+		if splitText.isEmpty {
+			//pop and delete
+			print("empty")
+			return
+		} else if splitText.count == 1 {
+			//only title and only content 구분 x ㅠㅠ
+			print("1")
+			memoTitle = String(splitText[0])
+			memoContent = ""
+		} else {
+			print("2")
+			memoTitle = String(splitText[0])
+			memoContent = String(splitText[1])
+ 		}
+		
+		let task = UserMemo(title: memoTitle, content: memoContent, date: date)
+		
+		
+		try! localRealm.write {
+			localRealm.add(task)
+		}
+		
+	}
+	
+	@objc func finishButtonClicked(_ sender: UIBarButtonItem) {
+		realmSave()
+		print(#function)
+	}
 	
 }
 
--- SeSacMemo/MemoApp/MainViewController.swift
+++ SeSacMemo/MemoApp/MainViewController.swift
@@ -10,21 +10,23 @@ import RealmSwift
 
 class MainViewController: UIViewController {
 	
-	//삭제 필요
-	let exarr = [1,2,3,4,5]
-	
 	@IBOutlet weak var mainTableView: UITableView!
 	@IBOutlet weak var mainToolbar: UIToolbar!
 	
 	@IBOutlet weak var editToolbarButton: UIBarButtonItem!
 	
 	let localRealm = try! Realm()
+	
 	var tasks: Results<UserMemo>!
+	var pinTasks: Results<UserMemo>! 
 	
 	override func viewDidLoad() {
         super.viewDidLoad()
 		
 		print("realmLocated:", localRealm.configuration.fileURL!)
+		
+		tasks = localRealm.objects(UserMemo.self).filter("pin == %@", false)
+		pinTasks = localRealm.objects(UserMemo.self).filter("pin == %@",true)
 		
 		mainTableView.delegate = self
 		mainTableView.dataSource = self
@@ -68,9 +70,9 @@ extension MainViewController: UITableViewDelegate, UITableViewDataSource {
 	
 	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
 		if section == 0 {
-			return 5
+			return pinTasks.count
 		} else {
-			return 10
+			return tasks.count
 		}
 		
 	}
@@ -88,19 +90,37 @@ extension MainViewController: UITableViewDelegate, UITableViewDataSource {
 		guard let cell = tableView.dequeueReusableCell(withIdentifier: "MainTableViewCell", for: indexPath) as? MainTableViewCell else {return UITableViewCell()}
 		
 		if indexPath.section == 0 {
-			cell.titleLabel.text = "title고정"
+			let row = pinTasks[indexPath.row]
+			cell.titleLabel.text = row.title
 			cell.titleLabel.font = .boldSystemFont(ofSize: 20)
-			cell.contentLabel.text = "content"
-			cell.dateLabel.text = "date"
+			cell.contentLabel.text = row.content
+			cell.dateLabel.text = row.date
 			
 			return cell
 		} else {
-			cell.titleLabel.text = "title"
+			let row = tasks[indexPath.row]
+			cell.titleLabel.text = row.title
 			cell.titleLabel.font = .boldSystemFont(ofSize: 20)
-			cell.contentLabel.text = "content"
-			cell.dateLabel.text = "date"
+			cell.contentLabel.text = row.content
+			cell.dateLabel.text = row.date
 		
 			return cell
+		}
+	}
+	
+	func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+		if indexPath.section == 0 {
+			let row = indexPath.row
+			let sb = UIStoryboard.init(name: "Content", bundle: nil)
+			guard let vc = sb.instantiateViewController(withIdentifier: "ContentViewController") as? ContentViewController else {return}
+			
+			self.navigationController?.pushViewController(vc, animated: true)
+		} else {
+		let row = indexPath.row
+		let sb = UIStoryboard.init(name: "Content", bundle: nil)
+		guard let vc = sb.instantiateViewController(withIdentifier: "ContentViewController") as? ContentViewController else {return}
+		
+		self.navigationController?.pushViewController(vc, animated: true)
 		}
 	}
 	
@@ -110,6 +130,9 @@ extension MainViewController: UITableViewDelegate, UITableViewDataSource {
 	}
 	
 	func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+		if pinTasks.count == 0 && section == 0{
+			return 0
+		}
 		return 35
 	}
 	
@@ -119,19 +142,24 @@ extension MainViewController: UITableViewDelegate, UITableViewDataSource {
 	
 	func tableView(_ tableView: UITableView, leadingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
 		let section = indexPath.section
-//		let row = indexPath.row
 		
 		if section == 0 {
 			let cancelPin = UIContextualAction(style: .normal, title: nil) { (cancelPin, view, completionHandler) in
-				print("\(indexPath) canclepin")
+				let pinRow = self.pinTasks[indexPath.row]
+				try! self.localRealm.write {
+					pinRow.pin.toggle()
+				}
+				tableView.reloadData()
 			}
 			cancelPin.image = UIImage(systemName: "pin.slash.fill")
 			cancelPin.backgroundColor = .orange
 			
 			return UISwipeActionsConfiguration(actions: [cancelPin])
 		} else {
+			let row = self.tasks[indexPath.row]
 			let pin = UIContextualAction(style: .normal, title: nil) { (pinAction, view, completionHandler) in
-				if self.exarr.count == 5 {
+				// action handler
+				if self.pinTasks.count == 5 {
 					let alert = UIAlertController(title: "죄송합니다.", message: "무료버전은 5개밖에 고정되지 않아요 ㅜ.ㅜ \n월 1990원으로 프리미엄 혜택을 누려보세요!", preferredStyle: .alert)
 					
 					let cancle = UIAlertAction(title: "cancle", style: .cancel) { action in
@@ -143,9 +171,14 @@ extension MainViewController: UITableViewDelegate, UITableViewDataSource {
 					self.present(alert, animated: true, completion: nil)
 
 				} else {
-					print("\(indexPath) pin")
+					// 5개 아닐때 추가 가능
+					print(row.pin)
+					try! self.localRealm.write {
+						row.pin.toggle()
+					}
+					print(row.pin)
+					tableView.reloadData()
 				}
-				
 			}
 			pin.image = UIImage(systemName: "pin.fill")
 			pin.backgroundColor = .orange
